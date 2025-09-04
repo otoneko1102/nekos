4b5ab4d8487a840bc473073cf421d1a3e4555d5f
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-09-04 10:54:01 +0900

    Add version check.

--- bin/nekos.js
+++ bin/nekos.js
@@ -2,15 +2,32 @@
 
 import { program } from "commander";
 import nekos from "../dist/index.js";
+import { consola } from "consola";
+import { isPackageLatest } from "is-package-latest";
+import pkg from "../package.json" with { type: "json" };
+
+async function versionInfo(p) {
+  const res = await isPackageLatest(p);
+  if (!res.isLatest)
+    consola.warn(
+      `New version released! =^･w･^=\n\n${res.currentVersion} --> ${res.latestVersion}\n\nnpm install -g ${res.name}@latest`,
+    );
+  return p.version;
+}
 
 program
+  .version(
+    await versionInfo(pkg),
+    "-v, --version",
+    "Check the current version.",
+  )
   .option(
     "-i, --id <id>",
-    "Display a specific cat by its ID (filename without .txt)"
+    "Display a specific cat by its ID (filename without .txt)",
   )
   .option(
     "-c, --colors [colors...]",
-    'Specify colors (e.g., RANDOM, RAINBOW, #ff00ff, "#ff0000 #00ff00")'
+    'Specify colors (e.g., RANDOM, RAINBOW, #ff00ff, "#ff0000 #00ff00")',
   );
 
 program.parse(process.argv);
@@ -24,7 +41,7 @@ if (options.id) {
 
 if (options.colors) {
   const finalColors = options.colors.flatMap((colorString) =>
-    colorString.split(" ")
+    colorString.split(" "),
   );
 
   if (finalColors.length === 1) {
